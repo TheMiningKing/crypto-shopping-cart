5daf957a5df2f0562465f194ea92bffadc99b466
Daniel Bidulock <daniel@capitolhill.ca>
2018-02-10 13:16:51 -0500

    Get POST /cart route

--- routes/cart.js
+++ routes/cart.js
@@ -3,14 +3,27 @@
 const express = require('express');
 const router = express.Router();
 const models = require('../models');
+const Cart = require('../lib/cart');
 
 /**
- * Get /
+ * GET /
  */
 router.get('/', (req, res) => {
   res.render('cart', {
     pageTitle: 'crypto-shopping-cart',
     cart: null
+  });
+});
+
+/**
+ * POST /
+ */
+router.post('/', (req, res) => {
+  models.Product.findOne({_id: req.body.id}).then(prod => {
+    Cart.addToCart(prod, req.session.cart);
+    res.redirect('/cart');
+  }).catch(err => {
+    res.redirect('/');
   });
 });
 
--- spec/features/indexSpec.js
+++ spec/features/indexSpec.js
@@ -12,7 +12,13 @@ describe('index', () => {
 
   let browser;
 
-  it('adds a session on first visit', (done) => {
+  afterEach((done) => {
+    models.dropDatabase(() => {
+      done();
+    });
+  });
+
+  it('adds a session containing an empty cart on first visit', (done) => {
     browser = new Browser({ waitDuration: '30s', loadCss: false });
 
     models.collection('sessions').find({}).toArray((err, results) => {
@@ -29,6 +35,13 @@ describe('index', () => {
             done.fail(err);
           }
           expect(results.length).toEqual(1);
+          expect(results[0]._id).not.toBe(undefined);
+          expect(results[0].session).not.toBe(undefined);
+          expect(results[0].session.cookie).not.toBe(undefined);
+          expect(results[0].session.cart).not.toBe(undefined);
+          expect(results[0].session.cart.items).toEqual([]);
+          expect(results[0].session.cart.totals).toEqual(0);
+          expect(results[0].expires).not.toBe(undefined);
           done();
         });
       });
@@ -64,12 +77,6 @@ describe('index', () => {
       });
     });
 
-    afterEach((done) => {
-      models.dropDatabase(() => {
-        done();
-      });
-    });
-
     it('does not display empty DB messge', () => {
       browser.assert.elements('p.alert.alert-info', 0);
     });
@@ -84,13 +91,13 @@ describe('index', () => {
         browser.assert.text('ul#products li.product:nth-child(1) h3.product-title', results[0].name);
         browser.assert.element(`ul#products li.product figure.product-image img[src="/images/products/${results[0].image}"]`);
         browser.assert.text('ul#products li.product:nth-child(1) .cart-data form div span.price', results[0].price);
-        browser.assert.text(`ul#products li.product:nth-child(1) .cart-data form div input[value="${results[0].id}"]`);
+        browser.assert.text(`ul#products li.product:nth-child(1) .cart-data form div input[type=hidden][name=id][value="${results[0].id}"]`);
 
         // Second product
         browser.assert.text('ul#products li.product:nth-child(2) h3.product-title', results[1].name);
         browser.assert.element(`ul#products li.product figure.product-image img[src="/images/products/${results[1].image}"]`);
         browser.assert.text('ul#products li.product:nth-child(2) .cart-data form div span.price', results[1].price);
-        browser.assert.text(`ul#products li.product:nth-child(2) .cart-data form div input[value="${results[1].id}"]`);
+        browser.assert.text(`ul#products li.product:nth-child(2) .cart-data form div input[type=hidden][name=id][value="${results[1].id}"]`);
  
         done();
       }).catch((error) => {
@@ -99,7 +106,36 @@ describe('index', () => {
     });
 
     describe('adding item to cart', () => {
+      let product;
 
+      beforeEach((done) => {
+        models.Product.findOne({}).then((results) => {
+          product = results;
+          done();
+        });
+      });
+
+      it('adds an item to the cart session', (done) => {
+        browser.pressButton('form:nth-child(1) div button[type=submit]', () => {
+          models.collection('sessions').find({}).toArray((err, results) => {
+            if (err) {
+              done.fail(err);
+            }
+            expect(results.length).toEqual(1);
+            expect(results[0].session.cart.items.length).toEqual(1);
+            expect(results[0].session.cart.totals).toEqual(product.price);
+            done();
+          });
+        });
+      });
+
+      it('redirects to cart and displays order', (done) => {
+        browser.pressButton('form:nth-child(1) div button[type=submit]', () => {
+          browser.assert.redirected();
+          browser.assert.url('/cart');
+          done();
+        });
+      });
     });
   });
 });
