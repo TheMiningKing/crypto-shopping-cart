f4600edb7956396aeb853083264573bd44c426cc
Daniel Bidulock <daniel@capitolhill.ca>
2018-04-09 21:34:42 -0600

    Get link to main site from category page

--- spec/features/categoriesSpec.js
+++ spec/features/categoriesSpec.js
@@ -73,7 +73,7 @@ describe('categories', () => {
     });
 
     it('displays a Continue Shopping button linking to root', () => {
-      browser.assert.elements('i.fa.fa-shopping-cart.go-to-shop-lnk', 1);
+      browser.assert.elements('i.fa.fa-backward.go-to-shop-lnk', 1);
       browser.assert.link('.navbar-brand', 'See all products', '/');
     });
   });
@@ -108,10 +108,20 @@ describe('categories', () => {
       });
     });
 
-    it('displays a Checkout button', (done) => {
+    it('displays a Checkout link', (done) => {
       browser.clickLink('Checkout', (err) => {
         if (err) done.fail(err);
         browser.assert.success();
+        browser.assert.url('/cart');
+        done();
+      });
+    });
+
+    it('displays a Main Shop link', (done) => {
+      browser.clickLink('See all products', (err) => {
+        if (err) done.fail(err);
+        browser.assert.success();
+        browser.assert.url('/');
         done();
       });
     });
