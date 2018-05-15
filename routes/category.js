c7959975bc6d0262c5360bc550a261dd7a9fe282
Daniel Bidulock <daniel@capitolhill.ca>
2018-05-15 12:49:54 -0600

    Ensure session, retrieve wallets, and populate prices in products

--- routes/category.js
+++ routes/category.js
@@ -3,7 +3,6 @@
 const express = require('express');
 const router = express.Router();
 const models = require('../models');
-//const Cart = require('../lib/cart');
 
 /**
  * GET /
@@ -12,29 +11,38 @@ router.get('/:category', (req, res) => {
   if(!req.session.cart) {
     req.session.cart = {
       items: [],
-      totals: 0
+      totals: 0,
+      preferredCurrency: process.env.PREFERRED_CURRENCY
     };
-  }  
+  }
 
-  models.Product.find({ categories: req.params.category }, (err, results) => {
-    if (err) {
-      req.flash('error', [ { message: 'Something went wrong' } ]);
-    }
+  models.Wallet.find({}).then((wallets) => {
+    let preferredWallet;
+    wallets.some((wallet) => {
+      if (wallet.currency === req.session.cart.preferredCurrency) {
+        preferredWallet = wallet;
+        return true;
+      }
+      return false;
+    });
 
-    models.Product.find({ categories: req.params.category }).sort('createdAt').then((products) => {
+    models.Product.find({ categories: req.params.category }).populate('prices.wallet').sort('createdAt').then((products) => {
       if (!products.length) {
         req.flash('info', `No such category exists: ${req.params.category}`);
       }
 
       res.render('index', {
+        cart: req.session.cart,
         path: req.originalUrl,
         products: products,
-        messages: req.flash()
+        messages: req.flash(),
+        wallets: wallets
       });
     }).catch((error) => {
       return res.status(500).send(error);
     });
-
+  }).catch((error) => {
+    return res.status(500).send(error);
   });
 });
 
